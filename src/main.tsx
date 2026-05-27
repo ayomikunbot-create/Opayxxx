650a4e1025a12a4f09f632779a04e2c58f170fd5
ayomikunbot-create <ayomikunbot@gmail.com>
2026-05-27 23:22:23 +0100

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+
+import "./styles.css";
+import { getRouter } from "./router";
+
+const router = getRouter();
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element #root was not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
