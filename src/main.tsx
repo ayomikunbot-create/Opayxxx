cfdd56f2245e536921d6d8fd0a8b8638de089a12
ayomikunbot-create <ayomikunbot@gmail.com>
2026-05-27 23:21:43 +0100

    Delete src/main.tsx

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
-
-function App(){ return <div /> }
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
